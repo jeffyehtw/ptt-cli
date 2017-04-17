0659477eb43aa003a923f542dbea94f3f2f26b6b
noname0930 <noname19930930@gmail.com>
2017-04-17 21:58:05 +0800

    rename ptt.py to login.py

---
+++ login.py
@@ -0,0 +1,34 @@
+#coding=utf-8
+import os
+import sys
+import time
+import json
+import telnetlib
+
+from module import Ptt
+
+# constant
+PATH = os.path.dirname(os.path.abspath(__file__))
+
+def load_config():
+	with open(PATH + '/config.json', 'r') as config_file:
+		config = json.load(config_file)
+	return config
+		 
+if __name__=='__main__' :
+	# var
+	configs = None
+
+	# init
+	try:
+		configs = load_config()
+	except:
+		print('Missing config file')
+		exit()
+
+	# run script
+	for i in configs:
+		ptt = Ptt.Object(i)
+		ptt.login()
+		ptt.logout()
+		time.sleep(10)
