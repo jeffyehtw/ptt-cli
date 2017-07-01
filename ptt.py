739f539184efdda08d9a845996ad9e6078552d93
noname0930 <noname19930930@gmail.com>
2017-07-02 02:23:36 +0800

    test

---
+++ ptt.py
@@ -0,0 +1,9 @@
+#coding=utf-8
+import os
+import sys
+import time
+import json
+import telnetlib
+
+from module import Ptt
+
---
+++ ptt/cli.py
@@ -0,0 +1,80 @@
+import argparse
+
+# constant
+__version__ = '1.0'
+__description__ = 'ptt-cli - a command line tool for ptt.cc'
+__epilog__ = 'Report bugs to <cjyeh@cs.nctu.edu.tw>'
+
+def parse_argv():
+	# init
+	parser = argparse.ArgumentParser(
+		description=__description__,
+		epilog=__epilog__
+	)
+
+	# main parser
+	parser.add_argument(
+		'-v', '-V', '--version', 
+		action='version', 
+		help='Print program version', 
+		version='v{}'.format(__version__)
+	)
+
+	# sub parser
+	subparsers = parser.add_subparsers(
+		help='sub-command help'
+	)
+
+	# login
+	parser_login = subparsers.add_parser(
+		'login'
+	)
+	parser_login.add_argument(
+		'account'
+	)
+	parser_login.add_argument(
+		'password'
+	)
+
+	# post
+	parser_post = subparsers.add_parser(
+		'post'
+	)
+	parser_post.add_argument(
+		'account'
+	)
+	parser_post.add_argument(
+		'password'
+	)
+	parser_post.add_argument(
+		'--board',
+		required=True
+	)
+	parser_post.add_argument(
+		'-t', '--type',
+		required=True
+	)
+	parser_post.add_argument(
+		'-a', '--article',
+		required=True
+	)
+	parser_post.add_argument(
+		'-c', '--content',
+		default=' '
+	)
+	parser_post.add_argument(
+		'-f', '--file'
+	)
+
+	results = parser.parse_args()
+	
+	return {
+	}
+
+def main():
+	args = parse_argv()
+
+
+
+if __name__ == '__main__':
+	main()
